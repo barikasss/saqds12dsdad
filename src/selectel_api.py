10443a404c8570ea3c9c91cb708cf2d0575cb09c
barikasss <test@devopsik.com>
2026-05-01 21:14:12 +0300

    revert: back to working auth

--- src/selectel_api.py
+++ src/selectel_api.py
@@ -104,7 +104,7 @@ class SelectelClient:
                             }
                         },
                     },
-                    **({"scope": {"project": {"id": self._project_id}}} if self._project_id else {}),
+                    "scope": {"domain": {"name": self._account_id}},
                 }
             }
             try:
@@ -222,7 +222,6 @@ class SelectelClient:
             merged = {"X-Auth-Token": self._auth(), **caller_headers}
             if self._project_id:
                 merged["X-Auth-Project"] = self._project_id
-                merged["X-Project-Id"] = self._project_id
             try:
                 return self._request_with_retry(
                     method, url,
